ae769268b7759e5b0a58879d398fbd536bc60c02
Jeremy Louie <jeremysf74@yahoo.com>
2021-07-16 16:01:17 -0700

    Added home bar for mobile
    Only tested for index.html

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,15 @@
 
+// change background color
 window.onload = function() {
     document.body.style.backgroundColor = "#15181b";
+}
+
+
+function toggleMenuBar() {
+    var mobilePageContainer = document.getElementById("mobile_pages_container_1");
+    if (mobilePageContainer.style.display === "block") {
+        mobilePageContainer.style.display = "none";
+    } else {    // if menu bar selected
+        mobilePageContainer.style.display = "block";
+    }
 }
