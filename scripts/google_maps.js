3d35b0ec87053e40971ff4a10a9a531f74afb320
Jeremy Louie <jeremysf74@yahoo.com>
2021-07-23 12:00:02 -0700

    Small styling changes to Google Maps

--- scripts/google_maps.js
+++ scripts/google_maps.js
@@ -3,8 +3,9 @@ function initMap() {
     // coord locations of Engineering IV
     const labLocation = { lat: 34.069632890358406, lng: -118.4445256020825 };    
     const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 4,
+          zoom: 17.5,
           center: labLocation,
+          mapId: "72bd48810ad41574"
     });
 
     // The marker, positioned at Engineering IV 
