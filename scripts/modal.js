b8306c5dd37340336c3055d9c3b22e88c8cb70c3
Jeremy Louie <jeremysf74@yahoo.com>
2021-07-26 17:04:47 -0700

    Changes to modal popup

--- scripts/modal.js
+++ scripts/modal.js
@@ -2,10 +2,8 @@
 // links: https://webgilde.com/en/analytics-opt-out/
 //        https://developers.google.com/analytics/devguides/collection/gtagjs/user-opt-out
 
-var disableStr = "ga-disable-UA-199894373-1" 
-
 // closes popup button
-function closeAndOptOut() {
+function optOut() {
     // close + return to landing page
     document.cookie = disableStr + "=true expires=Thu, 31 Dec 2099 23:59:59 UTC path=/"
     window[disableStr] = true
@@ -14,19 +12,19 @@ function closeAndOptOut() {
     gtag("js", new Date())
     gtag("config", "UA-199894373-1", { "send_page_view": false })
 
-    document.getElementById("modal").style.display = "none"
+    document.getElementById("modal").style.visibility = "hidden"
     localStorage.setItem("show_popup", "false")
 }
 
 // google analytics set by default is property not set  
-function closeAndOptIn() {
+function optIn() {
     // close + return to landing page
     window[disableStr] = false
     window.dataLayer = window.dataLayer || []
     function gtag(){dataLayer.push(arguments)}
     gtag("js", new Date())
     gtag("config", "UA-199894373-1")
-    document.getElementById("modal").style.display = "none"
+    document.getElementById("modal").style.visibility = "hidden"
     localStorage.setItem("show_popup", "false")
 }
 
@@ -54,21 +52,21 @@ if (findCookie(disableStr + "=true") == true) {   // if tracking disabled
     1) check if "show_popup" has value true OR DNE --> show popup
        i)  if user clicks "accept" --> enable GA tracking cookies + close modal
        ii) else if user clicks "decline" --> enable GA non-tracking cookies + close modal
-    2) (else) check if "show_popup=true" has value false --> don"t show popup
+    2) (else) check if "show_popup=true" has value false --> don't show popup
        i)  reassign "show_popup=true" to "show_popup=false"
 */
 
-// on landing page load
+var disableStr = "ga-disable-UA-199894373-1" 
 window.onload = function() {
-    // if "show_popup" doesn't exist in local storage --> create it, initially set it to false 
-    if (localStorage.getItem("show_popup") === null) {
-        localStorage.setItem("show_popup", "true")
+    if (localStorage.getItem("show_popup") === null) {      // if "show_popup" doesn't exist in local storage
+        localStorage.setItem("show_popup", "true")          // create it, initially set it to false 
     }
 
     if (localStorage.getItem("show_popup") === "true") {            // if "show_popup" has value true 
-        document.getElementById("modal").style.display = "block"   // show modal
+        document.getElementById("modal").style.visibility = "visible"    // show modal
+        alert("happend")
     } else {                                                        // if "show_popup" has value false
-        document.getElementById("modal").style.display = "none"    // don't show popup
-        localStorage.setItem("show_popup", "false")                // reassign "show_popup=true" to "show_popup=false"
+        document.getElementById("modal").style.visibility = "hidden"     // don't show popup
+        localStorage.setItem("show_popup", "false")                 // reassign "show_popup=true" to "show_popup=false"
     }
 }
