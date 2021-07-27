33a79dbe2ed9babe948a10c04a0b51de797c155b
Jeremy Louie <jeremysf74@yahoo.com>
2021-07-26 20:30:27 -0700

    New script to begin tracking on other pages

--- scripts/modal.js
+++ scripts/modal.js
@@ -35,23 +35,6 @@ function findCookie(cookie_name) {
     return false
 }
 
-/*
-    if opt in, create local stroage cookie item
-    check if that item exists
-    if it does, enable tracking
-    otherwise, do nothing
-    call this function in all webpages
-*/
-function checkAndTrack() {
-    if (localStorage.getItem("start_tracking") === "true") {
-        window[disableStr] = false
-        window.dataLayer = window.dataLayer || []
-        function gtag(){dataLayer.push(arguments)}
-        gtag("js", new Date())
-        gtag("config", "UA-199894373-1")
-    }
-}
-
 // check if tracking enabled / disabled
 if (findCookie(disableStr + "=true") == true) {   // if tracking disabled
     window[disableStr] = true
---
+++ scripts/tracking.js
@@ -0,0 +1,22 @@
+/*
+    if opt in, create local stroage cookie item
+    check if that item exists
+    if it does, enable tracking
+    otherwise, do nothing
+    call this function in all webpages
+*/
+
+function checkAndTrack() {
+    if (localStorage.getItem("start_tracking") === "true") {
+        window[disableStr] = false
+        window.dataLayer = window.dataLayer || []
+        function gtag(){dataLayer.push(arguments)}
+        gtag("js", new Date())
+        gtag("config", "UA-199894373-1")
+        alert("called")
+    }
+}
+
+window.onload = function() {
+    checkAndTrack()
+}
