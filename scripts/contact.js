ee5b7c5ef2b2e32a7513031749d2c41c698a57a6
Jeremy Louie <jeremysf74@yahoo.com>
2021-08-01 12:20:06 -0700

    Back end for contact

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,19 +1,6 @@
 
-emailjs.init('user_pqO3HtclZTUFDOwnE797p')
-
 window.onload = function() {
-  document.getElementById('contact_form').addEventListener('submit', (event) => {
-    event.preventDefault()
-    // generate a five digit number for the contact_number variable
-    this.contact_number.value = (Math.random() * 100000) | 0
-    // these IDs from the previous steps
-    emailjs.sendForm('contact_service', 'contact_form', this).then(
-      function () {
-        console.log('SUCCESS!')
-      },
-      function (error) {
-        console.log('FAILED...', error)
-      }
-    )
-  })
+    document.getElementById("contact_form").addEventListener("submit", function(event) {
+        event.preventDefault();
+    }
 }
