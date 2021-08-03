2234e639d5684c51d887bfac83725c59b9870171
Jeremy Louie <jeremysf74@yahoo.com>
2021-08-02 19:51:14 -0700

    Small changes to Google Maps and contact page

--- scripts/google_maps.js
+++ scripts/google_maps.js
@@ -1,16 +1,20 @@
 
-function initMap() {
-    // coord locations of Engineering IV
-    const labLocation = { lat: 34.069632890358406, lng: -118.4445256020825 };    
-    const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 17.5,
-          center: labLocation,
-          mapId: "72bd48810ad41574"
-    });
+function initMap() {}
 
-    // The marker, positioned at Engineering IV 
-    const marker = new google.maps.Marker({
-        position: labLocation,
-        map: map,
-    });
-}
+$(() => {
+    initMap = function() {
+        // coord locations of Engineering IV
+        const labLocation = { lat: 34.069632890358406, lng: -118.4445256020825 };    
+        const map = new google.maps.Map(document.getElementById("map"), {
+              zoom: 17.5,
+              center: labLocation,
+              mapId: "72bd48810ad41574"
+        });
+
+        // The marker, positioned at Engineering IV 
+        const marker = new google.maps.Marker({
+            position: labLocation,
+            map: map,
+        });
+    }
+})
