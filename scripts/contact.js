2759e4228f61d25333efe812729bed34eed19c21
Jeremy Louie <jeremysf74@yahoo.com>
2021-08-02 19:36:59 -0700

    Testing changes

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,6 +1,22 @@
 
 window.onload = function() {
+    emailjs.init("user_pqO3HtclZTUFDOwnE797p")
+
+    var submitButton = document.getElementById("submit");
     document.getElementById("contact_form").addEventListener("submit", function(event) {
         event.preventDefault();
-    }
+        submitButton.value = "Sending...";
+
+        const serviceID = "default_service";
+        const templateID = "contact_form";
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                submitButton.value = "Send Email"
+                alert("Email successfully sent")
+            }, (err) => {
+                submitButton.value = "Send Email"
+                alert(JSON.stringify(err));
+            })
+    })
 }
