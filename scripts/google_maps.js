608fed8a25e907ac9269b1a68089b7d672b12317
Jeremy Louie <jeremysf74@yahoo.com>
2021-09-15 14:51:02 -0700

    Name and map changes

--- scripts/google_maps.js
+++ scripts/google_maps.js
@@ -3,7 +3,7 @@ function initMap() {
     // coord locations of Engineering IV
     const labLocation = { lat: 34.069632890358406, lng: -118.4445256020825 };    
     const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 17.5,
+          zoom: 13.5,
           center: labLocation,
           mapId: "72bd48810ad41574"
     });
